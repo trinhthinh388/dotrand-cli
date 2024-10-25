4d53d36e91f9dd6e05f2743512ede66a8d4ba449
Thinh Trinh Duc <thinh.duc.trinh@gmail.com>
2024-10-25 20:34:47 +0700

    Added phone generator command

---
+++ src/commands/phone.ts
@@ -0,0 +1,75 @@
+import { Command, ux } from '@oclif/core';
+import {
+  PHONE_METADATA,
+  SUPPORTED_PHONE_COUNTRIES,
+} from '../constants/phone.js';
+import ReRegExp from 'reregexp';
+import Table from 'cli-table3';
+import { parsePhoneNumber } from 'libphonenumber-js';
+
+export default class Phone extends Command {
+  static override description = 'Generate a random phone';
+
+  static override examples = [
+    '<%= config.bin %> <%= command.id %> <country code>',
+  ];
+
+  static strict = false;
+
+  private checkSupportedCountries(country: string) {
+    return SUPPORTED_PHONE_COUNTRIES.includes(country);
+  }
+
+  private generateRandomPhone(country: string) {
+    const { phoneCode, pattern } = PHONE_METADATA.find(
+      (metadata) => metadata.countryCode === country
+    )!;
+    const r = new ReRegExp(pattern);
+    const e164 = `+${phoneCode}${r.build()}`;
+    const parsed = parsePhoneNumber(e164);
+    return {
+      countryCode: country,
+      e164,
+      international: parsed.formatInternational(),
+      national: parsed.formatNational(),
+    };
+  }
+
+  public async run(): Promise<void> {
+    const { argv } = await this.parse(Phone);
+
+    if (!argv || argv.length === 0) {
+      this.log(
+        ux.colorize(this.config.theme?.flagRequired, '[ERROR]'),
+        'Required at least one country code'
+      );
+    }
+
+    const countries = (argv as string[]).map((c) => c.toUpperCase());
+
+    if (!this.checkSupportedCountries(countries[0])) {
+      this.log(
+        ux.colorize(this.config.theme?.flagRequired, '[ERROR]'),
+        `Country with country code ${ux.colorize(
+          this.config.theme?.json?.key,
+          countries[0]
+        )} is not supported.`
+      );
+    }
+
+    const table = new Table({
+      head: ['Country Code', 'E.164', 'International', 'National'],
+      style: { head: ['yellow'] },
+    });
+    const generated = this.generateRandomPhone(countries[0]);
+    table.push({
+      [generated.countryCode]: [
+        generated.e164,
+        generated.international,
+        generated.national,
+      ],
+    });
+
+    this.log(table.toString());
+  }
+}
